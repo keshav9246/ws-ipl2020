8bd297af3dfc45774953d9f69d74496c1dea6fea
Keshav Rajput <keshav.rajput@walmart.com>
2020-09-03 13:27:41 -0500

    adding equals to scorepk

--- src/main/java/com/cricket/wsipl2020/model/ScorePK.java
+++ src/main/java/com/cricket/wsipl2020/model/ScorePK.java
@@ -1,12 +1,32 @@
 package com.cricket.wsipl2020.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Embeddable;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
+@Getter
+@Setter
 public class ScorePK implements Serializable {
 
     private Integer gameNum;
     private String playerName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ScorePK)) return false;
+        ScorePK that = (ScorePK) o;
+        return getGameNum() == that.getGameNum()
+                && getPlayerName() == (that.getPlayerName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGameNum(),getPlayerName());
+    }
 }
